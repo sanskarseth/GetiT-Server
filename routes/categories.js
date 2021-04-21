82e3c17b4fbce8ef05b0a125fcd366f747df0980
Sanskar <sethsanskar@gmail.com>
2021-04-21 14:19:46 +0530

    updated auth

--- routes/categories.js
+++ routes/categories.js
@@ -21,7 +21,7 @@ router.get('/:id', validateObjectId, async (req, res) => {
 	res.send(category);
 });
 
-router.post('/', [auth], async (req, res) => {
+router.post('/', async (req, res) => {
 	const { error } = validate(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
 
